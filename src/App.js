97ef0602a47b06c62ca3d2106b613cc0a6fe2ff7
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-03-11 23:32:45 +0100

    first lines of code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FriendList />
+      hi
     </div>
   );
 }
 
-export default App;
+function FriendList() {
+  return (
+    <div className="friend-list">
+      <Friend />
+      <AddFriend />
+    </div>
+  );
+}
+function Friend() {
+  return (
+    <div className="friend-wrap">
+      <img
+        className="friend-avatar"
+        src="https://via.placeholder.com/100"
+        alt="friend"
+      />
+      <h1 className="friend-title">Friend Name</h1>
+      <p className="friend-bill-split">bill details</p>
+      <button className="friend-select">select</button>
+    </div>
+  );
+}
+function AddFriend() {
+  return <form className="add-friend-form">add friend</form>;
+}
+function Split() {
+  return <form>? split</form>;
+}
