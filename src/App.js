493ebb999b7701965898fe158ea19a936726e7f3
Mihai Butnariu <mihai.alex.butnariu@gmail.com>
2024-04-05 10:06:15 +0200

    dev-1.1.2

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ export default function App() {
   return (
     <div className="App">
       <FriendList />
+      <Split />
     </div>
   );
 }
@@ -35,7 +36,7 @@ function FriendList() {
       key={friend.id}
       name={friend.name}
       balance={friend.balance}
-      pic={friend.pic}
+      pic={pic}
     />
   ));
 
@@ -46,6 +47,7 @@ function FriendList() {
     </div>
   );
 }
+
 function Friend({ name, balance, pic }) {
   return (
     <div className="friend-wrap">
@@ -56,9 +58,35 @@ function Friend({ name, balance, pic }) {
     </div>
   );
 }
+
 function AddFriend() {
-  return <form className="add-friend-form"></form>;
+  return (
+    <form className="add-friend-form">
+      <label className="form-label">Friend name</label>
+      <input className="form-input" type="text" placeholder="Name" />
+      <label className="form-label">Image URL</label>
+      <input className="form-input" type="text" placeholder="Balance" />
+      <button className="form-add">Add</button>
+    </form>
+  );
 }
+
 function Split() {
-  return <form>? split</form>;
+  return (
+    <form className="split-bill-wrap">
+      <h1 className="split-bill-title">Split a bill with Ben</h1>
+      <label className="split-bill-label">Bill value</label>
+      <input className="wplit-bill-input" type="text" />
+      <label className="split-bill-label">Your expense</label>
+      <input className="wplit-bill-input" type="text" />
+      <label className="split-bill-label">ben's expense</label>
+      <input className="wplit-bill-input" type="text" />
+      <label className="split-bill-label">Who is paying the bill</label>
+      <select>
+        <option>Ben</option>
+        <option>You</option>
+      </select>
+      <button className="split-bill-button">Split Bill</button>
+    </form>
+  );
 }
